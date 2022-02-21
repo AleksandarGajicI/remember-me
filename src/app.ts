5ff6fd2f9d80fcb52c6b95808a70b0b308d6defb
AleksandarGajicI <aleksandar.gajic8070@gmail.com>
2022-02-21 23:51:28 +0100

    chore: deleted log

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,6 @@ app.post("/choose", (req: Request, res: Response) =>
 app.post("/sendEmail", (req: Request, res: Response) => {
   const { option, email } = req.body;
 
-  console.log(option, email);
   return sendEmail(option)(email)
     .then((_) => {
       res.status(200);
