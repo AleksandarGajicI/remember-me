652470e7356fdd98dff0a32148a8a4c435efc267
AleksandarGajicI <aleksandar.gajic8070@gmail.com>
2022-02-21 22:27:54 +0100

    fix: changed to dynamic port

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.info("Server listening...");
 });
 
